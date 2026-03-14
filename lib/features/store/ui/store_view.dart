55c9d3f9f472e6d2a336b6450898731ccf02258e
Khaled Bahjat <khaledbahjat0@gmail.com>
2026-03-14 12:08:18 -0700

    Refactor StoreView to implement a ListView layout and enhance UI with welcome message and search functionality

--- lib/features/store/ui/store_view.dart
+++ lib/features/store/ui/store_view.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:store_app/core/widgets/custom_search_feild.dart';
+import 'package:store_app/core/widgets/proudct_list.dart';
+import 'package:store_app/core/widgets/spacing_widget.dart';
 
 class StoreView extends StatelessWidget {
   const StoreView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(child: Container(child: Text('Testing Store View'))),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListView(
+        children: [
+          Center(
+            child: Text(
+              'Welcome To Our Store',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ),
+          heightSp(height: 20),
+          CustomSearchFeild(),
+          heightSp(height: 10),
+          ProudctList(),
+        ],
+      ),
     );
   }
 }
