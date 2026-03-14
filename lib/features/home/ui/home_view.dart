9fe0a0b2af2d955aebe084b2cf378c2613a9cb73
Khaled Bahjat <khaledbahjat0@gmail.com>
2026-03-14 11:03:09 -0700

    Enhance HomeView layout with category display and styling improvements

--- lib/features/home/ui/home_view.dart
+++ lib/features/home/ui/home_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:store_app/core/theme/app_colors.dart';
 import 'package:store_app/core/widgets/custom_search_feild.dart';
 import 'package:store_app/core/widgets/spacing_widget.dart';
 
@@ -13,9 +15,55 @@ class HomeView extends StatelessWidget {
         children: [
           CustomSearchFeild(),
           heightSp(height: 20),
-          Image.asset('assets/images/buy.jpg'),
+          Image.asset('assets/images/buy.jpg', height: 200, fit: BoxFit.cover),
+          heightSp(height: 15),
+          Text(
+            'Popular Categories',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          heightSp(height: 15),
+          SizedBox(
+            child: SizedBox(
+              height: 120.h,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: categories.length,
+                itemBuilder: (context, index) => Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: AppColors.kPrimaryColor,
+                        foregroundColor: AppColors.kWhiteColor,
+                        radius: 30,
+                        child: Icon(categories[index].icon, size: 40),
+                      ),
+                      heightSp(height: 5),
+                      Text(categories[index].name),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
+}
+
+List<CategoryItem> categories = [
+  CategoryItem(name: 'Sports', icon: Icons.sports),
+  CategoryItem(name: 'Electronics', icon: Icons.electrical_services),
+  CategoryItem(name: 'Colection', icon: Icons.collections),
+  CategoryItem(name: 'Books', icon: Icons.book),
+  CategoryItem(name: 'Games', icon: Icons.gamepad),
+  CategoryItem(name: 'Devlober', icon: Icons.developer_board),
+];
+
+class CategoryItem {
+  final String name;
+  final IconData icon;
+
+  CategoryItem({required this.name, required this.icon});
 }
