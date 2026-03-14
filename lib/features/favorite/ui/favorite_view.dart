dd524c8dcf7ec950d198aa1fa14253ebd62f09ef
Khaled Bahjat <khaledbahjat0@gmail.com>
2026-03-14 12:10:01 -0700

    Refactor FavoriteView to implement a ListView layout and display favorite products

--- lib/features/favorite/ui/favorite_view.dart
+++ lib/features/favorite/ui/favorite_view.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:store_app/core/widgets/proudct_list.dart';
+import 'package:store_app/core/widgets/spacing_widget.dart';
 
 class FavoriteView extends StatelessWidget {
   const FavoriteView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(child: Container(child: Text('Testing Favorite View'))),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListView(
+        children: [
+          Center(
+            child: Text(
+              'Your Favorite Products',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ),
+          heightSp(height: 20),
+          ProudctList(),
+        ],
+      ),
     );
   }
 }
