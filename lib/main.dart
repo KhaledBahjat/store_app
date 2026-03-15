943abfd7ee301507b6666f52155e3c61e4bf33ec
Khaled Bahjat <khaledbahjat0@gmail.com>
2026-03-15 01:21:51 -0700

    Refactor main.dart to initialize Supabase with async main function; update pubspec.yaml to include supabase_flutter dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:store_app/core/routing/router_generatour.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://xtmhgoctgsnltpyrjyvm.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh0bWhnb2N0Z3NubHRweXJqeXZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1NTY1ODIsImV4cCI6MjA4OTEzMjU4Mn0.V6Up4nx_GJK_6tqpxqZXrkQhj60oIeLxcPVJM8srdCQ',
+  );
   runApp(const MarketApp());
 }
 
